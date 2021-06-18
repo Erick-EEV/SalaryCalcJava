f66f77220fb058efab6125f4cd55813b2b2c5ce6
Erick-EEV <EEVERICKEEV@gmail.com>
2021-06-18 12:26:58 -0400

    Instructions

---
+++ salaryCalc.java
@@ -0,0 +1,5 @@
+//Function should ask user for how many hours worked
+//Then ask what is the hourly rate of employee
+//Then calculate weekly check
+//If over 40 hours worked give bonus of 500 dollars
+//print out reciept for weekly check and bonus
